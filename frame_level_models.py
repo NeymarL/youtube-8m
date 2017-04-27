e4153d324561b89724e103f9dfa123b22f366a68
NeymarL <lh57506155@qq.com>
2017-04-28 07:53:50 +0800

    no reuse

--- frame_level_models.py
+++ frame_level_models.py
@@ -132,28 +132,7 @@ class RCNNCell(tf.contrib.rnn.BasicLSTMCell):
 
     def __call__(self, inputs, state, scope=None):
         print(inputs)
-        inputs = tf.reshape(inputs, [-1, 32, 32])
-        inputs = tf.expand_dims(inputs, 3)
-        with slim.arg_scope([slim.conv2d], padding='SAME',
-                               weights_initializer=tf.truncated_normal_initializer(stddev=0.01),
-                               weights_regularizer=slim.l2_regularizer(0.0005),
-                               normalizer_fn=slim.batch_norm):
-          net = slim.conv2d(inputs, 32, [3, 3], scope='conv1')
-          net = slim.relu(net, 32, scope='relu1')
-          net = slim.max_pool2d(net, [2, 2], scope='pool1')
-          net = slim.conv2d(net, 64, [3, 3], scope='conv2')
-          net = slim.relu(net, 64, scope='relu2')
-          net = slim.max_pool2d(net, [2, 2], scope='pool2')
-          net = slim.conv2d(net, 128, [3, 3], scope='conv3')
-          net = slim.relu(net, 128, scope='relu3')
-          net = slim.max_pool2d(net, [2, 2], scope='pool3')
-          net = slim.conv2d(net, 256, [3, 3], scope='conv4')
-          net = slim.relu(net, 256, scope='relu4')
-          net = slim.max_pool2d(net, [2, 2], scope='pool4')
-          net = slim.conv2d(net, 512, [3, 3], scope='conv5')
-          net = slim.relu(net, 512, scope='relu5')
-          net = slim.max_pool2d(net, [2, 2], scope='pool5')
-          net = tf.squeeze(net, [1, 2])
+        net = CNNs(inputs, False)
         print(net)
         return super(RCNNCell, self).__call__(net, state, scope)
 
